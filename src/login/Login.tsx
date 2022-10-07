4dd6e19239fa49b0799b4345b9a422cc7c1496dd
ludinhquan <ludinhquan@gmail.com>
2022-10-08 01:01:24 +0700

    setup packages

---
+++ src/login/Login.tsx
@@ -0,0 +1,10 @@
+import classes from "./login.module.css"
+
+export const Login = () => {
+  return (
+    <div className="pt-4">
+    <div className={classes.tilte}>login page</div>
+    </div>
+  )
+}
+
